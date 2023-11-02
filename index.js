533c04695218c8aec12e4fcd2394eeddb6b8f499
Sokolova-LV <leyla-@ukr.net>
2023-11-02 21:45:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+/*const birthdayDate = new Date('2024-08-05');
+
+function daysUntilBirthdayDate(birthdayDate) {
+    const currentDate = new Date();
+    const timeUntilBirthdayDate = birthdayDate - currentDate;
+    const daysUntilBirthdayDate = Math.floor(timeUntilBirthdayDate / (1000 * 60 * 60 * 24));
+    return daysUntilBirthdayDate;
+};
+
+const countdownElement = document.getElementById('countdown');
+
+function updateCountdown() {
+    const days = daysUntilBirthdayDate(birthdayDate);
+    countdownElement.textContent = `До дня рождения Лейки осталось ${days} дней! Время готовить подарки!`;
+};
+
+updateCountdown();
+setInterval(updateCountdown, 1000);*/
